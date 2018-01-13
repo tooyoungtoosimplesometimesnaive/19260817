7ab47b739615d580cd2674040967a893ef623445
Xing <xingmarc@yeah.net>
2018-01-13 00:35:50 -0800

    Revise algorithm.

--- partition_string.cpp
+++ partition_string.cpp
@@ -8,44 +8,32 @@ using std::endl;
 using std::vector;
 using std::string;
 
-/*
-For a string that is n characters long, there are n - 1 positions.
-*/
-vector<vector<int>> vv(0);
-void d(vector<int> & v, int step, int size) {
-	if (step == size) {
-		vector<int> v_c(v);
-		vv.push_back(v_c);
+
+void dfs(string & s, int step) {
+	if (step == static_cast<int>(s.size()) - 1) {
+		cout << s << endl;
 		return;
 	}
-	v[step] = 1;
-	d(v, step + 1, size);
-	v[step] = 0;
-	d(v, step + 1, size);
+	dfs(s, step + 1);
+	s = s.substr(0, step + 1) + " " + s.substr(step + 1);
+	dfs(s, step + 2);
+	s = s.substr(0, step + 1) + s.substr(step + 2);
 }
 
 int main() {
 	string s0 {"a"};
 	string s1 {"abcd"};
+	string s2 {"abcde"};
+	string s3 {"abcdef"};
 
-
-	vector<int> v(0);
-	decltype (s1.size()) i = 0;
-	while(i < s1.size() - 1) {
-		v.push_back(0);
-		i++;
-	}
-	for (auto i: v)
-		cout << i;
-	cout << endl;
-
-	d(v, 0, static_cast<int> (v.size()));
-	cout << "size :" << vv.size() << endl;
-	for (auto v: vv)
-		cout << "P :";
-		for (auto i: v)
-			cout << i;	
-		cout << endl;
-
+	cout << "------" << endl;
+	dfs(s0, 0);
+	cout << "------" << endl;
+	dfs(s1, 0);
+	cout << "------" << endl;
+	dfs(s2, 0);
+	cout << "------" << endl;
+	dfs(s3, 0);
+	
 	return 0;
 }
